a86f506df50fefcb580537dfd856b430661802cf
Lyes <lyes.sadmi@tictactrip.eu>
2019-06-17 04:46:23 +0200

    inmem implem aircraft sheet provider - refacto tests

--- implem/inmem/AircraftSheetProviderRepo_test.go
+++ implem/inmem/AircraftSheetProviderRepo_test.go
@@ -11,26 +11,19 @@ import (
 )
 
 func Test_AircraftSheetProviderRepo_FindByName(t *testing.T) {
+	asp := inmem.NewAircraftSheetProviderRepo().Add(domain.NewAircraftSheet(
+		0, "", "", "", "", "", "", "Aero Boero AB-95/115/150/180", "", "", "",
+	)).Add(domain.NewAircraftSheet(
+		1, "", "", "", "", "", "", "Aeronca 11 Chief", "", "", "",
+	)).Add(domain.NewAircraftSheet(
+		2, "", "", "", "", "", "", "ODD_ONE", "", "", "",
+	))
 	t.Run("should return no AirCraftSheets", func(t *testing.T) {
-		asp := inmem.NewAircraftSheetProviderRepo().Add(domain.NewAircraftSheet(
-			0, "", "", "", "", "", "", "Aero Boero AB-95/115/150/180", "", "", "",
-		)).Add(domain.NewAircraftSheet(
-			1, "", "", "", "", "", "", "Aeronca 11 Chief", "", "", "",
-		))
-
 		got := asp.FindByName("NOTHING")
 		assert.Len(t, got, 0)
 	})
 
 	t.Run("should return matching name's AirCraftSheets", func(t *testing.T) {
-		asp := inmem.NewAircraftSheetProviderRepo().Add(domain.NewAircraftSheet(
-			0, "", "", "", "", "", "", "Aero Boero AB-95/115/150/180", "", "", "",
-		)).Add(domain.NewAircraftSheet(
-			1, "", "", "", "", "", "", "Aeronca 11 Chief", "", "", "",
-		)).Add(domain.NewAircraftSheet(
-			2, "", "", "", "", "", "", "ODD_ONE", "", "", "",
-		))
-
 		got := asp.FindByName("aero")
 		assert.Len(t, got, 2)
 
@@ -41,23 +34,18 @@ func Test_AircraftSheetProviderRepo_FindByName(t *testing.T) {
 }
 
 func Test_AircraftSheetProviderRepo_FindByID(t *testing.T) {
-	t.Run("should return AirCraftSheets with id 1", func(t *testing.T) {
-		asp := inmem.NewAircraftSheetProviderRepo().Add(domain.NewAircraftSheet(
-			0, "", "", "", "", "", "", "Aero Boero AB-95/115/150/180", "", "", "",
-		)).Add(domain.NewAircraftSheet(
-			1, "", "", "", "", "", "", "Aeronca 11 Chief", "", "", "",
-		))
+	asp := inmem.NewAircraftSheetProviderRepo().Add(domain.NewAircraftSheet(
+		0, "", "", "", "", "", "", "Aero Boero AB-95/115/150/180", "", "", "",
+	)).Add(domain.NewAircraftSheet(
+		1, "", "", "", "", "", "", "Aeronca 11 Chief", "", "", "",
+	))
 
+	t.Run("should return AirCraftSheets with id 1", func(t *testing.T) {
 		got := asp.FindByID(1)
 		assert.Equal(t, got.ID, 1)
 	})
 
 	t.Run("should return no aircraftSheet (nil)", func(t *testing.T) {
-		asp := inmem.NewAircraftSheetProviderRepo().Add(domain.NewAircraftSheet(
-			0, "", "", "", "", "", "", "Aero Boero AB-95/115/150/180", "", "", "",
-		)).Add(domain.NewAircraftSheet(
-			1, "", "", "", "", "", "", "Aeronca 11 Chief", "", "", "",
-		))
 		ID := 5
 		assert.Nil(t, asp.FindByID(ID))
 	})
